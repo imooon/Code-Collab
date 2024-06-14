9173c32481156e7506404ea33bec0836d5e36b56
imooon <imonidharia@gmail.com>
2024-06-14 16:01:48 -0400

    removed 5051 from cors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://localhost:5051", "https://code-collab-3q14.onrender.com"],
+    origin: ["http://localhost:5173", "https://code-collab-3q14.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }
@@ -39,7 +39,7 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5051", "https://code-collab-3q14.onrender.com"],
+  origin: ["http://localhost:5173", "https://code-collab-3q14.onrender.com"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
